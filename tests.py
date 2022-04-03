e4c3a909462fcae4d5a4c94ebf1949ed800d5c23
nicky-LV <nickystnkvs@outlook.com>
2022-04-03 23:00:58 +0100

    Test for creating Stripe product

--- tests.py
+++ tests.py
@@ -1,5 +1,21 @@
+import json
+from fastapi.testclient import TestClient
+from .main import app
+from .schemas import StripeProduct
+
+
+client = TestClient(app)
+
+
 def test_create_checkout_session():
     pass
 
+
 def test_create_product():
-    pass
+    # No ID specified, Stripe will provide a random ID for this test product.
+    test_product = StripeProduct(name="test_product", description="test_product", shippable=True,
+                                 url="https://test_url.com")
+
+    response = client.get("/create-product", data=json.dumps(test_product.dict()))
+
+    assert response.status_code == 201
